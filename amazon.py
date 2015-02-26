9e547d92e479131c2122457bf546cdd520582960
macLai <laizewen01@gmail.com>
2015-02-26 18:36:17 +0900

    Update amazon.py
    测试。提交给SellerCenter数据。

--- amazon.py
+++ amazon.py
@@ -7,6 +7,11 @@ import requests
 import re
 import time
 import thread
+from requests.packages.urllib3.poolmanager import PoolManager, proxy_from_url
+from ssl import PROTOCOL_TLSv1
+import mechanize
+from ghost import Ghost
+import json
 
 path = {
 	"cn" : "http://www.amazon.cn/",
@@ -75,9 +80,117 @@ class Product:
 			pass
 			#self.title = soup.find(id = "title").contents
 		return 1
-		
 
+class MyAdapter(requests.adapters.HTTPAdapter):
+	def init_poolmanager(self, connections, maxsize, block):
+		self.poolmanager = PoolManager(num_pools=connections, maxsize=maxsize, ssl_version=PROTOCOL_TLSv1)
 
 if  __name__ == '__main__':
-	amazon = Amazon("jp", "abc")
+	#amazon = Amazon("jp", "abc")
+	# s = requests.session()
+	# s.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36"
+	# s.mount('https://', MyAdapter())
+	# s.get('https://www.amazon.co.jp/gp/sign-in.html',  verify=False)
 	
+	#login = s.get('https://sellercentral.amazon.co.jp/gp/homepage.html',  verify=False)
+	#soup = BeautifulSoup(login.content)
+	#widgetToken = soup.find(name = "widgetToken")['value']
+	#print widgetToken
+	#metadata1 = soup.find(name = "metadata1")['value']
+	#print metadata1
+	#data = {'username':'578044856@qq.com','password':'911526','widgetToken':widgetToken,'metadata1':metadata1}
+	#res=s.post('https://sellercentral.amazon.co.jp/ap/widget',data,  verify=False);
+	#print res.content
+
+	br = mechanize.Browser()  
+	br.set_debug_http(True)
+	br.set_handle_robots(False)  
+	br.addheaders = [("User-agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13"),
+				("Connection","keep-alive")]  
+	
+	sign_in = br.open('https://www.amazon.co.jp/gp/sign-in.html') 
+	
+	br.select_form(name="signIn")  
+	br["email"] = '578044856@qq.com' 
+	br["password"] = '911526'
+	logged_in = br.submit() 
+
+	orders_html = br.open("https://sellercentral.amazon.co.jp/hz/inventory?asin=B00B4MDR3U")
+	soup = BeautifulSoup(orders_html.read())
+	content = soup.find(attrs={"class":"a-bordered a-horizontal-stripes  mt-table"}).contents[1]
+	id1 = content.attrs["id"]
+	data = content.attrs["data-row-data"]
+
+	payload = {
+		"tableId":"myitable",
+		"updatedRecords":
+		[{
+			"recordId":id1,
+			"updatedFields":
+			[{
+				"fieldId":"quantity",
+				"changedValue":"50",
+				"beforeValue":"2"
+			}]
+		}],
+		"viewContext":
+		{
+			"action":"TABLE_SAVED",
+			"pageNumber":1,
+			"recordsPerPage":25,
+			"sortedColumnId":"Date",
+			"sortOrder":"DESCENDING",
+			"searchText":"B00B4MDR3U",
+			"tableId":"myitable",
+			"filters":[],
+			"clientState":
+			{
+				"recordsAboveTheFold":"25",
+				"enableMultiPageSelect":"true",
+				"confirmActionPageMaxRecords":"250",
+				"viewId":"DEFAULT"
+			}
+		}
+	}
+	print json.dumps(payload)
+	br.set_debug_http(True)
+	br.addheaders = [('Content-Type', 'application/json')]
+	br.open(mechanize.Request("https://sellercentral.amazon.co.jp/hz/inventory/save?ref_=xx_xx_save_xx",data=json.dumps(payload),headers={"Content-type":"application/json"}, ))
+	#br.open("https://sellercentral.amazon.co.jp/hz/inventory/save?ref_=xx_xx_save_xx", json.dumps(payload),headers={"Content-type":"application/json"}))
+
+# 	cookiejar = br._ua_handlers["_cookies"].cookiejar
+# 	print cookiejar
+# #	s = requests.session()
+# #	s.cookies = cookiejar
+# #	s.UserAgent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13"
+# #	print s.get('https://sellercentral.amazon.co.jp/gp/homepage.html',  verify=False).content
+
+
+# 	s = requests.Session()
+	
+# 	user_agent = "User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36" 
+# 	extra_headers = { 'User-Agent' : user_agent,"Content-Type" : "application/x-www-form-urlencoded" }
+# 	s.headers.update(extra_headers) 
+
+# 	r = s.get('https://www.amazon.co.jp/gp/sign-in.html', verify = False)
+# 	print r.cookies
+# 	soup = BeautifulSoup(r.content)
+# 	inputs = soup.find_all('input')
+
+# 	attributes = [i.attrs for i in inputs]
+# 	form_values = {}
+
+# 	for a in attributes:
+# 		print a.get('name')# + "   "+a.get('value')
+# 		form_values[a.get('name')] = a.get('value')
+
+# 	form_values['email'] = '578044856@qq.com'
+# 	form_values['password'] = "911526"
+# 	print s.headers
+# 	print s.cookies
+# 	s.cookies = cookiejar;
+# 	r = s.post( 'https://www.amazon.co.jp/ap/signin', data = form_values, verify = False, headers={"content-type" : "application/x-www-form-urlencoded","session-id-time":"2082726001l"})
+# 	print r.content 
+	
+# 	r = s.get('https://sellercentral.amazon.co.jp/gp/homepage.html', verify = False)
+# 	print r.content
